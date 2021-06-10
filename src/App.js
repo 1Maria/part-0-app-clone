80005974339c80bff7e61a28deebc84b4fca5a3b
full_name <email_address>
2021-06-10 15:54:42 -0400

    Add a piece of state to the App component that keeps track of which notes should be displayed

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import Note from './components/Note';
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes);
-  const [newNote, setNewNote] = useState(
-    'a new note...'
-  );
+  const [newNote, setNewNote] = useState('');
+  const [showAll, setShowAll] = useState(true);
 
   const addNote = (event) => {
     event.preventDefault();
