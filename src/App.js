d7739736617e769e659a23fac4afc5bf413599f6
full_name <email_address>
2021-07-08 13:05:24 -0400

    Add a button to every note to toggle importance

--- src/App.js
+++ src/App.js
@@ -35,6 +35,17 @@ const App = () => {
       });
   }
 
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3020/notes/${id}`
+    const note = notes.find(n => n.id === id);
+    const changedNote = { ...note, important: !note.important }
+
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(note => note.id !== id ? note : response.data))
+    });
+    // console.log('importance of ' + id + ' needs to be toggled');
+  }
+
   const handleNoteChange = (event) => {
     console.log(event.target.value);
     setNewNote(event.target.value);
@@ -54,7 +65,12 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map(note =>
-          <Note key={note.id} note={note} />)}
+          <Note 
+            key={note.id}  
+            note={note} 
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        )}
       </ul>
       <form onSubmit={addNote}>
         <input 
